799ddf70f86f433e7a01d33ecf315b4ced2dbc70
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-04 16:05:19 +0530

    Add files via upload

---
+++ clculator.js
@@ -0,0 +1,17 @@
+const Calculator = (A, B, C) => 
+{
+	switch(A){
+	  case '+':return B+C
+    break;
+    
+    case '-':return  B-C
+    break;
+    
+    case '*':return  B*C
+    break;
+    
+    case '/':return  B/C
+    
+	}
+    
+};
